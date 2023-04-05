dd1134db4eb1f5409ec970a17eb52a2530f9cdbf
mryevhenii07 <mrit.works07@gmail.com>
2023-04-05 10:06:14 +0300

    finished project

--- src/components/SearchCustomers/SearchCustomers.tsx
+++ src/components/SearchCustomers/SearchCustomers.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import s from './SearchCustomers.module.css';
 import SearchImg from '../../assets/images/search.png';
+
 interface Props {
   value: string;
   setValue: (str: string) => void;
@@ -12,7 +13,6 @@ const SearchCustomers: FC<Props> = ({ value, setValue }) => {
       <img src={SearchImg} alt="img" width={18} />
       <input
         type="text"
-        name=""
         value={value}
         onChange={(e) => setValue(e.target.value)}
         placeholder="search"
